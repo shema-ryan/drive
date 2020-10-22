8b4da307270c4e00419181c36b89b773689be6b6
shema-ryan <kamanzishema@gmail.com>
2020-10-22 16:21:41 +0300

    drive-app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -20,9 +21,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.brown,
         accentColor: Colors.brown[100],
       ),
-      routes: {
-        '/': (BuildContext context) => HomePage(),
-      },
+      home: HomePage(),
+      routes: {},
     );
   }
 }
@@ -33,8 +33,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey _form = GlobalKey<FormState>();
+  GlobalKey _scaffold = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      key: _scaffold,
+      body: Stack(
+        children: [
+          Container(
+            height: size.height,
+            width: size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        'https://www.turbinebd.com/wp-content/uploads/2020/01/ooo-1.jpg'))),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor.withOpacity(0.7),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              height: 300,
+              child: Card(
+                color: Colors.white54,
+                margin: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    key: _form,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(),
+                        TextFormField(),
+                        TextFormField(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
